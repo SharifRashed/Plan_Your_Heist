fe10ce2cb8c476c00bd6c234d4857ffc3147f615
Sharif Rashed <sharif.rashed26@gmail.com>
2022-03-03 14:07:19 -0600

    phase 1 complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan your Heist");
+            Console.WriteLine("Plan your Heist!");
+
+            Console.Write("What is your team member's name? ");
+            string TeamMemberName = Console.ReadLine();
+
+            Console.Write("What is your team member's skill? 1-50 ");
+            var TeamMemberSkill = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your team member's courage? 0.0- 2.0");
+            var TeamMemberCourage = decimal.Parse(Console.ReadLine());
+
+            Console.Write($"Team member's information: \n Team member name: {TeamMemberName}, \n Team member skill: {TeamMemberSkill}, \n Team member courage: {TeamMemberCourage}");
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,10 @@
+
+namespace Heist
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public int Skill { get; set; }
+        public double Courgage { get; set; }
+    }
+}
